12a79305d7ccd5a210ba12225302c302c0c88ce7
Christophe Pijcke <cpijcke.ff@laposte.net>
2019-10-08 20:58:43 +0200

    Add metrics.hpp who include every metric's

---
+++ include/metrics.hpp
@@ -0,0 +1,33 @@
+// -*- C++ -*-
+//
+//===---------------------------- metrics all -----------------------------===//
+//
+// Copyright (c) 2018, 2019, Christophe Pijcke
+//
+// This file was largely inspired by the chrono library
+//  from the LLVM Compiler Infrastructure.
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef METRICS_ALL_HPP
+#define METRICS_ALL_HPP
+
+#include "angularspeed.hpp"
+#include "energy.hpp"
+#include "mass.hpp"
+#include "power.hpp"
+#include "voltage.hpp"
+#include "distance.hpp"
+#include "force.hpp"
+#include "metric_config.hpp"
+#include "pressure.hpp"
+#include "volume.hpp"
+#include "electriccurrent.hpp"
+#include "frequency.hpp"
+#include "speed.hpp"
+#include "volumetricflow.hpp"
+
+#endif // METRICS_ALL_HPP
