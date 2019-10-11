04a198c2ce82da0618e69b14466eff93f65d3986
Christophe Pijcke <cpijcke.ff@laposte.net>
2019-10-11 18:06:10 +0200

    hmm

--- include/metric_config.hpp
+++ include/metric_config.hpp
@@ -15,15 +15,31 @@
 #include <ratio>
 
 
-#ifdef _WIN32
 
-#if _MSC_VER > 1800
-    #define METRICCONSTEXPR constexpr
+#ifdef _WIN32
+	#if _MSC_VER > 1800
+    	#define METRICCONSTEXPR constexpr
+	#else
+    	#define METRICCONSTEXPR
+	#endif
+	#define GCD std::_Gcd
 #else
-    #define METRICCONSTEXPR
+	#define METRICCONSTEXPR constexpr
+	#define GCD std::__static_gcd
 #endif
 
-#define GCD std::_Gcd
+
+#ifdef __GNUC__
+#define CHAR_BIT __CHAR_BIT__
+#endif
+
+
+#ifdef __clang__
+
+#define LCM std::__static_lcm
+
+#else
+
 
 // _Lcm is defined in chrono on Windows.  But we don't want to necessary include chrono when we are using metrics.
 namespace std
@@ -32,7 +48,7 @@ namespace std
 template <intmax_t _Xp, intmax_t _Yp>
 struct LCM
 {
-    static const intmax_t value = _Xp / _Gcd<_Xp, _Yp>::value * _Yp;
+    static const intmax_t value = _Xp / GCD<_Xp, _Yp>::value * _Yp;
 };
 
 template <class _Tp>                    struct __is_ratio                          : std::false_type {};
@@ -40,11 +56,6 @@ template <intmax_t _Num, intmax_t _Den> struct __is_ratio<std::ratio<_Num, _Den>
 
 } // namespace std
 
-#else
-
-#define METRICCONSTEXPR constexpr
-#define GCD std::__static_gcd
-#define LCM std::__static_lcm
 
 #endif
 
