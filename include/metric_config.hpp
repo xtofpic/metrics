5f860faffc1839bd1c2edea0bbc8157f80413f11
christophe <christophe@server.envali.com>
2019-10-12 16:07:19 +0200

    Correct compilation issue for clang on linux

--- include/metric_config.hpp
+++ include/metric_config.hpp
@@ -36,7 +36,31 @@
 
 #ifdef __clang__
 
+
+#ifdef __GNUC__
+
+// _Lcm is defined in chrono on Windows.  But we don't want to necessary include chrono when we are using metrics.
+namespace std
+{
+
+template <intmax_t _Xp, intmax_t _Yp>
+struct LCM
+{
+    static const intmax_t value = _Xp / GCD<_Xp, _Yp>::value * _Yp;
+};
+
+template <class _Tp>                    struct __is_ratio                          : std::false_type {};
+template <intmax_t _Num, intmax_t _Den> struct __is_ratio<std::ratio<_Num, _Den> > : std::true_type  {};
+
+} // namespace std
+
+#else
+
 #define LCM std::__static_lcm
+
+#endif
+
+
 
 #else
 
